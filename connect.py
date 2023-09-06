2b431d066498978969b56b7298e1d4d045712154
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-06 23:32:42 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,36 +1,35 @@
-from fastapi import FastAPI, HTTPException
+from flask import Flask, jsonify
 import psycopg2
 
 # Database connection details
-DB_HOST = "10.2.0.4"
-DB_PORT = "5432"
 DB_NAME = "omridb"
 DB_USER = "omri"
 DB_PASSWORD = "Hapoe_l6984"
+DB_HOST = "10.2.0.4"
+DB_PORT = 8080  # PostgreSQL default port
 
-app = FastAPI()
-
+app = Flask(__name__)
 
 try:
     connection = psycopg2.connect(
         host=DB_HOST,
-        port=DB_PORT,
         database=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD
     )
 except Exception as e:
-    raise HTTPException(status_code=500, detail=f"Error connecting to the database: {e}")
-    
-   
+    raise Exception(f"Error connecting to the database: {e}")
+
 cursor = connection.cursor()
 
-@app.get("/")
-async def get_clients():
-    cursor.execute("""SELECT * FROM clients""")
-    clients = cursor.fetchall()
-    return clients
+@app.route("/")
+def get_clients():
+    try:
+        cursor.execute("""SELECT * FROM clients""")
+        clients = cursor.fetchall()
+        return jsonify(clients)
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=DB_PORT, debug=True)
