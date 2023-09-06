280dc38db4b2b143beb7b52e0b5917edcde6f55f
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-06 23:59:10 +0300

    Update app_script.sh

--- Terraform/app_script.sh
+++ Terraform/app_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Update package lists
-sudo apt update
-sudo apt install nano
+sudo apt-get update 
 
 sudo apt install -y python3 python3-pip
 pip install flask 
-pip psycopg2-binary
+pip install psycopg2-binary
  
 
-sudo apt-get update 
 sudo apt install git -y  
 git clone https://github.com/omriyan01/terraform-fastapi-.git
-python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/connect.py
+sudo python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/connect.py
