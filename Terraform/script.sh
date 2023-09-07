7f14c2fb3e3d8372043374c8f1f28b400b9bef1d
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-07 17:07:37 +0300

    Update script.sh

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-
-# Update package lists
-sudo apt-get update
-
-# Install PostgreSQL and its dependencies
-sudo apt-get install -y postgresql postgresql-contrib
-
-# Start the PostgreSQL service
+apt-get update
+apt install postgresql postgresql-contrib -y
+# Start and enable the PostgreSQL service
 sudo systemctl start postgresql.service
 
-# Create a PostgreSQL database
-sudo -u postgres psql -c "CREATE DATABASE fastapi_db;"
+# Set a password for the default PostgreSQL user (postgres)
+sudo -u postgres psql -c "CREATE DATABASE omridb;"
+sudo -u postgres psql -c "CREATE USER omri PASSWORD 'Hapoe_l6984';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE omridb TO omri;"
+sudo -u postgres psql
+\c omridb
+CREATE TABLE clients (id SERIAL PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, role VARCHAR);
+INSERT INTO clients (first_name, last_name, role) VALUES ('John', 'Smith', 'CEO');
 
-# Create a PostgreSQL user and grant privileges on the database
-sudo -u postgres psql -c "CREATE USER omri WITH PASSWORD 'Hapoe_l6984';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE fastapi_db TO moshe;"
+# Replace '0.0.0.0/0' with your specific IP range or remove/comment this line for local access only
+echo "host all all 10.0.0.4/16 md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
 
+# Update listen_addresses to allow all incoming connections
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/*/main/postgresql.conf
 
-echo "listen_addresses = '*'" | sudo tee -a /etc/postgresql/*/main/postgresql.conf
-echo "host   all    all 10.0.0.4/24     md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
-sudo ufw allow 5432/tcp
+# Update port to 5432
+ sudo sed -i "s/port = 5434/port = 5432/g" /etc/postgresql/*/main/postgresql.conf
+
+# Reload PostgreSQL for changes to take effect
 sudo service postgresql restart
-
-#go into fastapi_db 
-sudo -u postgres psql -d fastapi_db
-
 
 
 
