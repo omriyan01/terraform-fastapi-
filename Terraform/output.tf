0c4262899ee01df6fb59bb5309f4d2938fee85f6
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-07 17:06:41 +0300

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,19 +1,15 @@
-output "vm_db_public_ip" {
-  description = "Public IP address of the database virtual machine"
-  value       = azurerm_public_ip.vm_db_public_ip.ip_address
+output "app_vm_private_ip" {
+  value = azurerm_network_interface.nic-omri-app.private_ip_address
 }
 
-output "vm_app_public_ip" {
-  description = "Public IP address of the application virtual machine"
-  value       = azurerm_public_ip.vm_app_public_ip.ip_address
+output "db_vm_private_ip" {
+  value = azurerm_network_interface.nic-omri-db.private_ip_address
 }
 
-output "vm_db_private_ip" {
-  description = "Private IP address of the database virtual machine"
-  value       = azurerm_network_interface.nic_omri_db.private_ip_address
+output "app_vm_public_ip" {
+  value = azurerm_public_ip.vm-web-public-ip.ip_address
 }
 
-output "vm_app_private_ip" {
-  description = "Private IP address of the application virtual machine"
-  value       = azurerm_network_interface.nic_omri_app.private_ip_address
+output "db_vm_public_ip" {
+  value = azurerm_public_ip.vm-db-public-ip.ip_address
 }
