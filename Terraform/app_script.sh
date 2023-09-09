38043176d803f6bee46850de49e0d734d09a1e7c
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-09 21:36:13 +0300

    Update app_script.sh

--- Terraform/app_script.sh
+++ Terraform/app_script.sh
@@ -11,6 +11,12 @@ pip install psycopg2-binary
 
 # Clone repo and run scripts
 sudo apt install git -y  
-git clone https://github.com/omriyan01/terraform-fastapi-.git
-sudo python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/blob/main/Terraform/app/python.py
-sudo python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/blob/main/Terraform/app/init_db.py
+sudo wget -P /home/adminuser https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/app/python.py
+sudo wget -P python.py /home/adminuser https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/app/python.py
+sudo wget -P /home/adminuser https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/app/init_db.py
+sudo wget -P init_db.py https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/app/init_db.py
+sudo mv /var/lib/waagent/custom-script/download/0/python.py /home/adminuser/
+wget init_db.py https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/app/init_db.py
+wget index.html https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/templates/index.html
+wget create.html https://raw.githubusercontent.com/omriyan01/terraform-fastapi-/main/Terraform/templates/create.html
+wget base.html https://github.com/omriyan01/terraform-fastapi-/blob/main/Terraform/templates/base.html
