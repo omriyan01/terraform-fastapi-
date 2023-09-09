3379455d62f7d994b9784c8c940bf0e6ecfed062
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-09 20:45:20 +0300

    Update script.sh

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -5,13 +5,9 @@ apt install postgresql postgresql-contrib -y
 sudo systemctl start postgresql.service
 
 # Set a password for the default PostgreSQL user (postgres)
-sudo -u postgres psql -c "CREATE DATABASE omridb;"
-sudo -u postgres psql -c "CREATE USER omri PASSWORD 'Hapoe_l6984';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE omridb TO omri;"
-sudo -u postgres psql
-\c omridb
-CREATE TABLE clients (id SERIAL PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, role VARCHAR);
-INSERT INTO clients (first_name, last_name, role) VALUES ('John', 'Smith', 'CEO');
+sudo -u postgres psql -c "CREATE USER omri WITH PASSWORD 'omri';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE omri_db TO moshe;"
+
 
 # Replace '0.0.0.0/0' with your specific IP range or remove/comment this line for local access only
 echo "host all all 10.0.0.4/16 md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
@@ -21,9 +17,11 @@ sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/po
 
 # Update port to 5432
  sudo sed -i "s/port = 5434/port = 5432/g" /etc/postgresql/*/main/postgresql.conf
+ sudo service postgresql restart
+ sudo systemctl start postgresql.service
+ 
 
 # Reload PostgreSQL for changes to take effect
 sudo service postgresql restart
-
 
 
