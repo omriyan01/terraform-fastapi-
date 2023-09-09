556ce55b8777facc733029d2b6bb94e7ea2253fe
omriyan01 <136433984+omriyan01@users.noreply.github.com>
2023-09-09 20:45:44 +0300

    Update app_script.sh

--- Terraform/app_script.sh
+++ Terraform/app_script.sh
@@ -12,4 +12,5 @@ pip install psycopg2-binary
 # Clone repo and run scripts
 sudo apt install git -y  
 git clone https://github.com/omriyan01/terraform-fastapi-.git
-sudo python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/connect.py
+sudo python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/blob/main/Terraform/app/python.py
+sudo python3 /var/lib/waagent/custom-script/download/0/terraform-fastapi-/blob/main/Terraform/app/init_db.py
